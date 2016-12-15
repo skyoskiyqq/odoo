af2a36405ef34b08fcf35726cb468b801381a28c
skyoskiyqq <huqianglingdu@163.com>
2016-12-15 08:27:51 +0800

    todo app init

---
+++ addons/todo_app/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'To-Do Application',
+    'version': '1.0',
+    'summary': 'Time',
+    'description': """
+Manage your time and work more effective.
+    """,
+    'depends': [],
+    'category': '',
+    'sequence': 13,
+    'demo': [],
+    'data': [],
+    'test': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
